74a74b41ec0c53b00f65a2db5ab95e8a3aeecad1
PESmereczniak <Philip3.8@hotmail.com>
2018-03-17 09:01:38 -0500

    Add project files.

---
+++ Where2PayLogin/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Where2PayLogin.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
